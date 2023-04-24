5f7f25041aacbe8e6d79e9bdc637e05442e37a79
Anton <antony.adamson@gmail.com>
2023-04-24 19:09:36 +0600

    промежуточный кормит

--- DC-LessonTSystemViewControllers-SystemViewControllers/Controller/ViewController.swift
+++ DC-LessonTSystemViewControllers-SystemViewControllers/Controller/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    //UIImagePickerControllerDelegate передаст информацию о выбранном изображении обратно в приложение.
+    //UINavigationControllerDelegate будет отвечать за закрытие представления выбора изображения.
 
     @IBOutlet weak var imageView: UIImageView!
     
@@ -39,26 +41,61 @@ class ViewController: UIViewController {
     }
     
     @IBAction func cameraButtonTapped(_ sender: UIButton) {
+        //создаем экземпляр класса UIImagePickerController
+        let imagePicker = UIImagePickerController()
+        //пусть экземпляр будет делагатом
+        imagePicker.delegate = self
+        
         //создаем экземпляр класса UIAlertController содержащий заголовок и тп
         let alertController = UIAlertController(title: "Choose image source", message: nil, preferredStyle: .actionSheet)
         //создаем экземпляры класса UIAlertAction
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        //экземпляр с кодом замыкания, которое будет выполнено при выборе
-        let cameraAction = UIAlertAction(title: "Camera", style: .default) { action in
-            print("User selected Camera action")
-        }
-        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { action in
-            print("User selected Photo Library action")
+        
+        //добавляем в alertController кнопку
+        alertController.addAction(cancelAction)
+        
+        //проверяем доступность источника данных - камера телефона
+        //ОБЯЗАТЕЛЬНО добавить в info.plist ключ NSCameraUsageDescription со значением строки - поясняющей пользователю, как приложение использует эти данные и зачем ему нужен доступ к камере
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            //если доступен то создаем экземпляр класса с замыканием при выборе
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { action in
+                //замыкание присваивает источнику данных экземпляра класса - камеру
+                imagePicker.sourceType = .camera
+                //и показывает это
+                self.present(imagePicker, animated: true)
+            }
+            //добавляем эту кнопку в alertController
+            alertController.addAction(cameraAction)
         }
         
-        //добавляем в alertController 3 кнопки
-        alertController.addAction(cancelAction)
-        alertController.addAction(cameraAction)
-        alertController.addAction(photoLibraryAction)
+        //аналогично предыдущему
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { action in
+                imagePicker.sourceType = .photoLibrary
+                self.present(imagePicker, animated: true)
+            }
+            alertController.addAction(photoLibraryAction)
+        }
+        
         //добавляем функциональность для больших экранов
         alertController.popoverPresentationController?.sourceView = sender
+        
         // выводим alertController на экран через present
         present(alertController, animated: true)
+        
+    }
+    
+    //метод для получения выбранного фото из библиотеки изображений устройства
+    //Этот метод сообщает делегату, что пользователь выбрал фото (или другой медиа-контент), и включает фото в словарь info
+    func imageControlPicker(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //Так как словарь info имеет тип [UIImagePickerController.InfoKey: Any], то значение для ключа originalImage не будет иметь тип UIImage. Необходимо привести тип значения к типу UIImage
+        //защита. если мы можем создать экземпляр из info[.originalImage] и привести к типу UIImage то:
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
+        
+        //то можем обновить изображение в imageView
+        imageView.image = selectedImage
+        //и закроем выбор изображения
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func emailButtonTapped(_ sender: Any) {
