80665f28f1c21ed144ed2b6e9a1c8f9e73b017bf
Anton <antony.adamson@gmail.com>
2023-04-24 17:25:45 +0600

    Добавлена функциональность в виде алертКонтроллера для кнопки "Camera"

--- DC-LessonTSystemViewControllers-SystemViewControllers/Controller/ViewController.swift
+++ DC-LessonTSystemViewControllers-SystemViewControllers/Controller/ViewController.swift
@@ -30,13 +30,35 @@ class ViewController: UIViewController {
     }
     
     @IBAction func safariButtonTapped(_ sender: UIButton) {
+        //если мы смогли создать экземпляр класса URL с параметрами то:
         if let url = URL(string: "https://www.apple.com") {
+            //можем создать экземпляр класса SFSafariViewController в который передадим нашу ссылку
             let safariViewController = SFSafariViewController(url: url)
             present(safariViewController, animated: true, completion: nil)
         }
     }
     
     @IBAction func cameraButtonTapped(_ sender: UIButton) {
+        //создаем экземпляр класса UIAlertController содержащий заголовок и тп
+        let alertController = UIAlertController(title: "Choose image source", message: nil, preferredStyle: .actionSheet)
+        //создаем экземпляры класса UIAlertAction
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        //экземпляр с кодом замыкания, которое будет выполнено при выборе
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { action in
+            print("User selected Camera action")
+        }
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { action in
+            print("User selected Photo Library action")
+        }
+        
+        //добавляем в alertController 3 кнопки
+        alertController.addAction(cancelAction)
+        alertController.addAction(cameraAction)
+        alertController.addAction(photoLibraryAction)
+        //добавляем функциональность для больших экранов
+        alertController.popoverPresentationController?.sourceView = sender
+        // выводим alertController на экран через present
+        present(alertController, animated: true)
     }
     
     @IBAction func emailButtonTapped(_ sender: Any) {
